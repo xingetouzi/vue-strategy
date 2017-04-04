dc1082db02aa57a682b3562e47ba251bec5cbfda
spider <492305218@qq.com>
2017-04-04 18:14:36 +0800

    开启sourcemap，在template中使用scss并引入外部scss配置文件。

--- src/components/header.vue
+++ src/components/header.vue
@@ -13,8 +13,53 @@
         </ul>
     </nav>
 </template>
-<style>
-
+<style lang="scss">
+    //必须要加~才行
+    @import "~css/variable.scss";
+    .header {
+        height: $header-height;
+        overflow: hidden;
+        background-color: $primary-color;
+        padding: 0 15px;
+        .nav-left{
+            height: 100%;
+            float: left;
+        }
+        .nav-logo, .nav-title {
+            display: table-cell;
+            height: $header-height;
+            color: white;
+            vertical-align: middle;
+        }
+        .nav-logo {
+            img {
+                height: 32px;
+                width: 136px;
+            }
+        }
+        .nav-title {
+            font-size: 20px;
+        }
+        > ul{
+            list-style: none;
+            float: right;
+            margin: 0;
+            padding: 0;
+            color: white;
+        }
+        .nav-setting{
+            height: 100%;
+            >li{
+                height: 100%;
+                line-height: $header-height;
+                padding: 0 10px;
+            }
+            >li:hover{
+                background-color: darken($primary-color,10%);
+                cursor: pointer;
+            }
+        }
+    }
 </style>
 <script>
    export default{
