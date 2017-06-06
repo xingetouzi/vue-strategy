5cd77e76c11bd2f5e5c40ed70c07aeed3c022f12
spider <492305218@qq.com>
2017-06-07 00:32:49 +0800

    feat: 策略选购页基本搞定。

--- src/page/shop/index.vue
+++ src/page/shop/index.vue
@@ -14,11 +14,14 @@
             <div class="grid-content bg-purple common-header-search">
               <el-input v-model="searchText" placeholder="请输入策略ID/名称"></el-input>
               <el-button @click="onSearch()">搜索</el-button>
+              <el-button type="primary" @click="isAddDialogShow = true">添加策略</el-button>
             </div>
           </el-col>
           <el-col :span="12">
             <div class="grid-content bg-purple-light common-header-import-button">
-              <el-button type="primary" @click="isAddDialogShow = true">添加策略</el-button>
+              <el-button>购买选中</el-button>
+              <el-button type="info" @click="onTryClick()">试用选中</el-button>
+              <el-button type="danger" @click="onDeleteClick()">删除选中</el-button>
             </div>
           </el-col>
         </el-col>
@@ -31,7 +34,9 @@
           <el-table
                   ref="shopTable"
                   :data="tableData"
-                  style="width: 100%">
+                  style="width: 100%"
+                  @selection-change="handleSelectionChange"
+          >
             <el-table-column
                     type="selection"
                     width="55">
@@ -51,12 +56,18 @@
             <el-table-column
                     prop="trialExpired"
                     label="试用期"
-            >
+                    :filters="[{ text: '可使用', value: '1' }, { text: '试用中', value: '2' },{ text: '即将到期', value: '3' },{ text: '已经到期', value: '4' }]"
+                    :filter-method="filterTrialExpired"
+                    filter-placement="bottom-start">
+              >
             </el-table-column>
             <el-table-column
                     prop="buyingExpired"
                     label="付费使用期"
-            >
+                    :filters="[{ text: '全部', value: '0' }, { text: '从未购买', value: '1' },{ text: '使用中', value: '2' },{ text: '即将过期', value: '3' },{ text: '已过期', value: '4' }]"
+                    :filter-method="filterBuyingExpired"
+                    filter-placement="bottom-start">
+              >
             </el-table-column>
           </el-table>
         </el-col>
@@ -64,9 +75,7 @@
       <div class="block-20"></div>
       <el-row>
         <el-col :span="24" class="tlr">
-          <span>已经选择10条记录</span>
-          <span class="space-10"></span>
-          <el-button>查看详情</el-button>
+          <span>已选择 <span :style="{color:'red'}">{{multipleSelection.length}}</span> 条记录</span>
         </el-col>
       </el-row>
     </div>
@@ -82,7 +91,7 @@
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="isAddDialogShow = false">取 消</el-button>
-        <el-button type="primary" @click="onDialogConfirm">确 定</el-button>
+        <el-button type="primary" @click="onAddDialogConfirm">确 定</el-button>
       </div>
     </el-dialog>
   </div>
@@ -95,11 +104,12 @@
     data(){
       return {
         tableData: [],
-        isAddDialogShow: false,
+        isAddDialogShow: false, // 添加策略
         searchText: '',
         strategyAddForm: {
           strategyText: ''
-        }
+        },
+        multipleSelection: [] // 多选
       }
     },
     mounted () { // 挂载后执行
@@ -116,10 +126,11 @@
       onSearch(){
         console.log('onSearch', this.searchText)
       },
-      async onDialogConfirm(){
-        let strategy = this.strategyAddForm.strategyText.split('，')
-        console.log('onDialogConfirm', strategy)
-        let res = await addPurchaseBill({strategy})
+      // 添加策略窗口确定
+      async onAddDialogConfirm(){
+        let strategies = this.strategyAddForm.strategyText.split('，')
+        console.log('onAddDialogConfirm', strategies)
+        let res = await addPurchaseBill({strategies})
         if (res.code === 200) {
           this.isAddDialogShow = false
           this.strategyAddForm.strategyText = ''
@@ -128,6 +139,33 @@
             this.tableData = res.data
           }
         }
+      },
+      onTryClick(){
+        console.log('onTryClick')
+        this.$confirm('选中且尚未试用的策略将进入试用期计时，之后您可以在控制台中查看并启动该策略，是否确认？', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '提交使用策略，跳转到控制台'
+          })
+        }).catch(() => {
+        })
+      },
+      onDeleteClick(){
+        console.log('onDeleteClick')
+      },
+      filterTrialExpired(){
+
+      },
+      filterBuyingExpired(){
+
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val
+        console.log(this.multipleSelection)
       }
     }
   }
